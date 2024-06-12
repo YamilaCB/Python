e662d8f1ad0c24438eb29a561695da4d73436ce1
YamilaCB <166716902+YamilaCB@users.noreply.github.com>
2024-06-12 11:30:03 -0300

    k

---
+++ ejercicio.pag169.py
@@ -0,0 +1,13 @@
+#decoracion
+print("-----------------------------------------------------")
+print(                "ejercicio.pag.169"                    )
+print("-----------------------------------------------------")
+#proceso
+inicio=2
+pausa=20
+salto=2 #incrementar de 2 en 2
+for i in range(inicio,pausa+1,salto):
+    c=i**3
+    print ("el cubo de",i,"es",c)
+    
+    
